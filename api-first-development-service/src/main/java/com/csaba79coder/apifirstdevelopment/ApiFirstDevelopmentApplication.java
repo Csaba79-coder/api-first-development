470474065bdfbd48370508e765d70f4568438873
Csaba79-coder <csabavadasz79@gmail.com>
2023-01-22 12:02:02 +0100

    Initial commit

---
+++ api-first-development-service/src/main/java/com/csaba79coder/apifirstdevelopment/ApiFirstDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.apifirstdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFirstDevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFirstDevelopmentApplication.class, args);
+    }
+
+}
---
+++ api-first-development-service/src/test/java/com/csaba79coder/apifirstdevelopment/ApiFirstDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.apifirstdevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFirstDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
