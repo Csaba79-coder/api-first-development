ddd0b186bb8fc69277884f5318f7522492a147a5
Csaba79-coder <csabavadasz79@gmail.com>
2023-01-22 17:48:42 +0100

    update contract for Book entity

--- api-first-development-service/src/main/java/com/csaba79coder/apifirstdevelopment/controller/BookController.java
+++ api-first-development-service/src/main/java/com/csaba79coder/apifirstdevelopment/controller/BookController.java
@@ -1,15 +1,32 @@
 package com.csaba79coder.apifirstdevelopment.controller;
 
-import com.csaba79coder.api.LibraryApi;
+import com.csaba79coder.api.BooksApi;
 import com.csaba79coder.models.Book;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 
-import java.util.List;
+import java.util.UUID;
 
-public class BookController implements LibraryApi {
+@Controller
+public class BookController implements BooksApi {
 
     @Override
-    public ResponseEntity<List<Book>> getAllBooksInLibrary() {
+    public ResponseEntity<Book> addBook(Book body) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteBook(UUID bookId, String apiKey) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Book> getBookById(UUID bookId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Book> updateBook(Book body) {
         return null;
     }
 }
