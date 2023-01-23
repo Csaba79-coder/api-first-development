040ce7aceb0841d8a78d5e63242f1de774f78169
Csaba79-coder <csabavadasz79@gmail.com>
2023-01-23 15:01:06 +0100

    create enums: Availability, Status and Genre (for Book entity)

---
+++ api-first-development-service/src/main/java/com/csaba79coder/apifirstdevelopment/value/Availability.java
@@ -0,0 +1,5 @@
+package com.csaba79coder.apifirstdevelopment.value;
+
+public enum Availability {
+    AVAILABLE, ARCHIVE, DELETED
+}
---
+++ api-first-development-service/src/main/java/com/csaba79coder/apifirstdevelopment/value/Status.java
@@ -0,0 +1,5 @@
+package com.csaba79coder.apifirstdevelopment.value;
+
+public enum Status {
+    AVAILABLE, PENDING, SOLD
+}
