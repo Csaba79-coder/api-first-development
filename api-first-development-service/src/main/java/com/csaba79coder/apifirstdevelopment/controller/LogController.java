92e01350387dd830a3d360a6f9902fc6dc8bfeeb
Csaba79-coder <csabavadasz79@gmail.com>
2023-01-23 16:47:25 +0100

    implementing LogApi

--- api-first-development-service/src/main/java/com/csaba79coder/apifirstdevelopment/controller/LogController.java
+++ api-first-development-service/src/main/java/com/csaba79coder/apifirstdevelopment/controller/LogController.java
@@ -1,12 +1,28 @@
 package com.csaba79coder.apifirstdevelopment.controller;
 
+import com.csaba79coder.api.LogsApi;
 import com.csaba79coder.apifirstdevelopment.service.LogService;
+import com.csaba79coder.models.LogModel;
+import com.csaba79coder.models.NewLogModel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class LogController {
+public class LogController implements LogsApi {
 
     private final LogService logService;
+
+    @Override
+    public ResponseEntity<LogModel> addLog(NewLogModel body) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<LogModel>> renderAllLogs() {
+        return null;
+    }
 }
